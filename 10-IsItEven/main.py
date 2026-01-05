3c14b1c24b967f89cacf1899628255d3ac6cdd01
afnan-codelab <afnanmmd1@gmail.com>
2026-01-05 19:08:59 +0300

    Add Exercise 10 - Is It Even

---
+++ 10-IsItEven/main.py
@@ -0,0 +1,10 @@
+def main():
+    number = int(input("Enter a number: "))
+
+    if number % 2 == 0:
+        print("The number is the even.")
+    else:
+        print("The number is odd.")
+
+if __name__ == "__main__":
+    main()
